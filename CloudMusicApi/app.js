19839f83a7e9d40a7dfab782ff899509c270325a
mumu <79960449+mumu-pika@users.noreply.github.com>
2022-06-20 22:11:54 +0800

    api
    网易云后台api

---
+++ CloudMusicApi/app.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+require('./server').serveNcmApi({
+  checkVersion: true,
+})
+
+
---
+++ CloudMusicApi/server.js
@@ -0,0 +1,361 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const request = require('./util/request')
+const packageJSON = require('./package.json')
+const exec = require('child_process').exec
+const cache = require('./util/apicache').middleware
+const { cookieToJson } = require('./util/index')
+const fileUpload = require('express-fileupload')
+const decode = require('safe-decode-uri-component')
+
+
+// 引入Fly.js这个http请求库
+const Fly = require('flyio/src/node')
+// 引入jsonwebtoken这个加密token库
+const jwt = require('jsonwebtoken');
+const fly = new Fly
+
+/**
+ * The version check result.
+ * @readonly
+ * @enum {number}
+ */
+const VERSION_CHECK_RESULT = {
+  FAILED: -1,
+  NOT_LATEST: 0,
+  LATEST: 1,
+}
+
+/**
+ * @typedef {{
+ *   identifier?: string,
+ *   route: string,
+ *   module: any
+ * }} ModuleDefinition
+ */
+
+/**
+ * @typedef {{
+ *   port?: number,
+ *   host?: string,
+ *   checkVersion?: boolean,
+ *   moduleDefs?: ModuleDefinition[]
+ * }} NcmApiOptions
+ */
+
+/**
+ * @typedef {{
+ *   status: VERSION_CHECK_RESULT,
+ *   ourVersion?: string,
+ *   npmVersion?: string,
+ * }} VersionCheckResult
+ */
+
+/**
+ * @typedef {{
+ *  server?: import('http').Server,
+ * }} ExpressExtension
+ */
+
+/**
+ * Get the module definitions dynamically.
+ *
+ * @param {string} modulesPath The path to modules (JS).
+ * @param {Record<string, string>} [specificRoute] The specific route of specific modules.
+ * @param {boolean} [doRequire] If true, require() the module directly.
+ * Otherwise, print out the module path. Default to true.
+ * @returns {Promise<ModuleDefinition[]>} The module definitions.
+ *
+ * @example getModuleDefinitions("./module", {"album_new.js": "/album/create"})
+ */
+async function getModulesDefinitions(
+  modulesPath,
+  specificRoute,
+  doRequire = true,
+) {
+  const files = await fs.promises.readdir(modulesPath)
+  const parseRoute = (/** @type {string} */ fileName) =>
+    specificRoute && fileName in specificRoute
+      ? specificRoute[fileName]
+      : `/${fileName.replace(/\.js$/i, '').replace(/_/g, '/')}`
+
+  const modules = files
+    .reverse()
+    .filter((file) => file.endsWith('.js'))
+    .map((file) => {
+      const identifier = file.split('.').shift()
+      const route = parseRoute(file)
+      const modulePath = path.join(modulesPath, file)
+      const module = doRequire ? require(modulePath) : modulePath
+
+      return { identifier, route, module }
+    })
+
+  return modules
+}
+
+/**
+ * Check if the version of this API is latest.
+ *
+ * @returns {Promise<VersionCheckResult>} If true, this API is up-to-date;
+ * otherwise, this API should be upgraded and you would
+ * need to notify users to upgrade it manually.
+ */
+async function checkVersion() {
+  return new Promise((resolve) => {
+    exec('npm info NeteaseCloudMusicApi version', (err, stdout) => {
+      if (!err) {
+        let version = stdout.trim()
+
+        /**
+         * @param {VERSION_CHECK_RESULT} status
+         */
+        const resolveStatus = (status) =>
+          resolve({
+            status,
+            ourVersion: packageJSON.version,
+            npmVersion: version,
+          })
+
+        resolveStatus(
+          packageJSON.version < version
+            ? VERSION_CHECK_RESULT.NOT_LATEST
+            : VERSION_CHECK_RESULT.LATEST,
+        )
+      }
+    })
+
+    resolve({
+      status: VERSION_CHECK_RESULT.FAILED,
+    })
+  })
+}
+
+/**
+ * Construct the server of NCM API.
+ *
+ * @param {ModuleDefinition[]} [moduleDefs] Customized module definitions [advanced]
+ * @returns {Promise<import("express").Express>} The server instance.
+ */
+
+// 服务器
+async function consturctServer(moduleDefs) {
+  const app = express()
+  app.set('trust proxy', true)
+
+  /**
+   * CORS & Preflight request
+   */
+  app.use((req, res, next) => {
+    if (req.path !== '/' && !req.path.includes('.')) {
+      res.set({
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': req.headers.origin || '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+        'Content-Type': 'application/json; charset=utf-8',
+      })
+    }
+    req.method === 'OPTIONS' ? res.status(204).end() : next()
+  })
+
+  /**
+   * Cookie Parser
+   */
+  app.use((req, _, next) => {
+    req.cookies = {}
+      //;(req.headers.cookie || '').split(/\s*;\s*/).forEach((pair) => { //  Polynomial regular expression //
+      ; (req.headers.cookie || '').split(/;\s+|(?<!\s)\s+$/g).forEach((pair) => {
+        let crack = pair.indexOf('=')
+        if (crack < 1 || crack == pair.length - 1) return
+        req.cookies[decode(pair.slice(0, crack)).trim()] = decode(
+          pair.slice(crack + 1),
+        ).trim()
+      })
+    next()
+  })
+
+  /**
+   * Body Parser and File Upload
+   */
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+
+  app.use(fileUpload())
+
+  /**
+   * Serving static files
+   */
+  app.use(express.static(path.join(__dirname, 'public')))
+
+  /**
+   * Cache
+   */
+  app.use(cache('2 minutes', (_, res) => res.statusCode === 200))
+
+
+  // 小程序可以通过微信官方提供的登录能力方便地获取微信提供的用户身份标识，快速建立小程序内的用户体系。
+  // 开发者服务器获取用户唯一标识的接口
+  // 发送数据:(登录凭证校验接口) appid + appsecret + code
+  // 返回数据: session_key + openid等
+
+  app.use('/getOpenId', async (req, res, next) => {
+    let code = req.query.code; //获取用户的登录凭证code
+    let appId = 'wxc18ae250deec9be9'; //AppID(小程序ID)
+    let appSecret = '394494f10db9a0b37105888fe7bceb46'; //AppSecret(小程序密钥)
+    let url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`
+    // 发请求给微信服务器获取openId
+    let result = await fly.get(url)
+    let openId = JSON.parse(result.data).openid
+
+    /* 
+      其实拿到用户openid,和session_key后，当前登录的账户就在微信的服务器中产生了登录态。
+      但是微信的官方文档提示开发者，为了安全起见，需要第三方服务器自己定义一个登陆态
+
+      1、开发者服务器可以根据用户标识来生成自定义登录态，用于后续业务逻辑中前后端交互时识别用户身份。
+      2、当开发者在实现自定义登录态时，可以考虑以session_key有效期作为自身登录态有效期，也可以实现自定义的时效性策略。
+    */
+    // 自定义登录态
+    let person = {
+      username: 'pika',
+      age: 22,
+      openId
+    }
+    // 对用户的数据进行加密，生成token返回给客户端
+    let token = jwt.sign(person, 'superPika') //jwt.sign(需要加密的对象, 密钥)
+    // 验证身份，反编译token
+    let trueToken = jwt.verify(token, 'superPika')
+    res.send(token)
+  })
+
+  /* 
+  小程序文档截取
+    说明：
+    1、调用 wx.login() 获取 临时登录凭证code ，并回传到开发者服务器。
+    2、调用 auth.code2Session 接口，换取 用户唯一标识 OpenID 、 用户在微信开放平台帐号下的唯一标识UnionID（若当前小程序已绑定到微信开放平台帐号） 和 会话密钥 session_key。
+    之后开发者服务器可以根据用户标识来生成自定义登录态，用于后续业务逻辑中前后端交互时识别用户身份。
+
+    注意事项：
+    1、会话密钥 session_key 是对用户数据进行 加密签名 的密钥。为了应用自身的数据安全，开发者服务器不应该把会话密钥下发到小程序，也不应该对外提供这个密钥。
+    2、临时登录凭证 code 只能使用一次
+  */
+
+
+  /**
+   * Special Routers
+   */
+  const special = {
+    'daily_signin.js': '/daily_signin',
+    'fm_trash.js': '/fm_trash',
+    'personal_fm.js': '/personal_fm',
+  }
+
+  /**
+   * Load every modules in this directory
+   */
+  const moduleDefinitions =
+    moduleDefs ||
+    (await getModulesDefinitions(path.join(__dirname, 'module'), special))
+
+  for (const moduleDef of moduleDefinitions) {
+    // Register the route.
+    app.use(moduleDef.route, async (req, res) => {
+      ;[req.query, req.body].forEach((item) => {
+        if (typeof item.cookie === 'string') {
+          item.cookie = cookieToJson(decode(item.cookie))
+        }
+      })
+
+      let query = Object.assign(
+        {},
+        { cookie: req.cookies },
+        req.query,
+        req.body,
+        req.files,
+      )
+
+      try {
+        const moduleResponse = await moduleDef.module(query, request)
+        console.log('[OK]', decode(req.originalUrl))
+
+        const cookies = moduleResponse.cookie
+        if (Array.isArray(cookies) && cookies.length > 0) {
+          if (req.protocol === 'https') {
+            // Try to fix CORS SameSite Problem
+            res.append(
+              'Set-Cookie',
+              cookies.map((cookie) => {
+                return cookie + '; SameSite=None; Secure'
+              }),
+            )
+          } else {
+            res.append('Set-Cookie', cookies)
+          }
+        }
+        res.status(moduleResponse.status).send(moduleResponse.body)
+      } catch (/** @type {*} */ moduleResponse) {
+        console.log('[ERR]', decode(req.originalUrl), {
+          status: moduleResponse.status,
+          body: moduleResponse.body,
+        })
+        if (!moduleResponse.body) {
+          res.status(404).send({
+            code: 404,
+            data: null,
+            msg: 'Not Found',
+          })
+          return
+        }
+        if (moduleResponse.body.code == '301')
+          moduleResponse.body.msg = '需要登录'
+        res.append('Set-Cookie', moduleResponse.cookie)
+        res.status(moduleResponse.status).send(moduleResponse.body)
+      }
+    })
+  }
+
+  return app
+}
+
+/**
+ * Serve the NCM API.
+ * @param {NcmApiOptions} options
+ * @returns {Promise<import('express').Express & ExpressExtension>}
+ */
+
+// 网易云接口服务器封装
+async function serveNcmApi(options) {
+  const port = Number(options.port || process.env.PORT || '3000')
+  const host = options.host || process.env.HOST || ''
+
+  const checkVersionSubmission =
+    options.checkVersion &&
+    checkVersion().then(({ npmVersion, ourVersion, status }) => {
+      if (status == VERSION_CHECK_RESULT.NOT_LATEST) {
+        console.log(
+          `最新版本: ${npmVersion}, 当前版本: ${ourVersion}, 请及时更新`,
+        )
+      }
+    })
+  const constructServerSubmission = consturctServer(options.moduleDefs)
+
+  const [_, app] = await Promise.all([
+    checkVersionSubmission,
+    constructServerSubmission,
+  ])
+
+  /** @type {import('express').Express & ExpressExtension} */
+  // 获取app实例
+  const appExt = app
+  appExt.server = app.listen(port, host, () => {
+    console.log(`server running @ http://${host ? host : 'localhost'}:${port}`)
+  })
+
+  return appExt
+}
+
+module.exports = {
+  serveNcmApi,
+  getModulesDefinitions,
+}
